27a47551660e2a22c5ec35d3fcae56267182c626
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2012-01-28 04:55:23 -0600

    Small update.

---
+++ inc/index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2011-2012 Cool Dude 2k - http://idb.berlios.de/
+    Copyright 2011-2012 Game Maker 2k - http://intdb.sourceforge.net/
+    Copyright 2011-2012 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
+    $FileInfo: index.php - Last Update: 01/24/2012 Ver. 2.0.0 RC 12 - Author: cooldude2k $
+*/
+
+require("../upc.php");
+?>
